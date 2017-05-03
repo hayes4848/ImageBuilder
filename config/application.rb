8e6dc801d4ceb7706ba498589ab652c9d2f31244
Andy Hayes <andythemanhayes@hotmail.com>
2017-05-03 13:41:59 -0600

    wip again

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ require "action_mailer/railtie"
 # require "active_resource/railtie" 
 require "rails/test_unit/railtie"
 require "sprockets/railtie" 
+require 'action_cable/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
