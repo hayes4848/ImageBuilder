354cf07b4f99c3e027491abb513692a7875866f6
Andy Hayes <andythemanhayes@hotmail.com>
2017-07-26 11:36:27 -0600

    port bind

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,8 @@ set :use_sudo,        true
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       '/opt/socialavatar'
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "tcp://127.0.0.1:3000"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
